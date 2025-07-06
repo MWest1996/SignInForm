6e4dc443f64ef23194efeccf040c7c8545abcad3
Marvin West <marvin.a.west@gmail.com>
2025-07-06 15:57:44 -0400

    starting work on coding the login prompts

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("#inputFields");
+const fName = document.querySelector("#fName");
+const lName = document.querySelector("#lName");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
